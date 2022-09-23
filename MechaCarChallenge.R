c3c24cee68124f02f3f1c9dab19067ef83cba899
Matthew Poor <poorfamily@Matthews-MacBook-Air-2.local>
2022-09-22 20:04:16 -0400

    addint final update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,7 +8,7 @@ library(tidyverse)
 library(dplyr)
 
 # 4. Import and read in the MechaCar_mpg.csv file as a dataframe.
-mecha_mpg <- read.csv(file='./Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
+mecha_mpg <- read.csv(file='./MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
 
 # 5. Perform linear regression using the lm() function. 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
